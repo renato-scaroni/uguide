af7fe7f1005e162562a92e87b4060179b1b3848d
Renato <renato.scaroni@gmail.com>
2014-11-13 00:51:33 -0200

    Creating the repo and pushing all the stuff

---
+++ main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class Place(ndb.Model):
+    latitude = ndb.FloatProperty()
+    longitude = ndb.FloatProperty()
+    name = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'orig' : (-23.558745, -46.731859, 'IMEUSP') ,
+            'waypts' : [(-23.572742, -46.696095, 'Shopping Eldorado')],
+            'dest' : (-23.561518, -46.656009, 'MASP')
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+class RegisterStuff(webapp2.RequestHandler):
+    def get(self):
+        lat = self.request.get("lat")
+        lon = self.request.get("lon")
+        name = self.request.get("name")
+        place = Place()
+        place.latitude = lat
+        place.longitude = lon
+        place.name = name
+        place.put()
+        self.response.write("Regitered "+ name + " at longitude:"+lon+" and latitude:"+lat)
+
+class ListStuff(webapp2.RequestHandler):
+    def get(self):
+        query = Place.query()
+        s = "Name - Latitude - Longitude <br>"
+        for p in query.fetch(10):
+            s += p.name + " , "
+            s += str(p.latitude) + " , "
+            s += str(p.longitude) + "<br>"
+        self.response.write(s)
+
+class RandomTest(webapp2.RequestHandler):
+    def get(self):
+        lat = -23.558745
+        lon = -46.731859
+        name = 'IMEUSP'
+        place = Place()
+        place.latitude = lat
+        place.longitude = lon
+        place.name = name
+        place.put()
+        lat = -23.561518
+        lon = -46.656009
+        name = 'MASP'
+        place = Place()
+        place.latitude = lat
+        place.longitude = lon
+        place.name = name
+        place.put()
+        lat = -23.572742
+        lon = -46.696095
+        name = 'Shopping Eldorado'
+        place = Place()
+        place.latitude = lat
+        place.longitude = lon
+        place.name = name
+        place.put()
+        self.response.write("Ok")
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/register', RegisterStuff),
+    ('/list', ListStuff),
+    ('/random', RandomTest)
+], debug=True)
