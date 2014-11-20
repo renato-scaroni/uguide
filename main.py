6b1f2d254ddb53377c564ccc28cfc2b52b0b71b0
Renato <renato.scaroni@gmail.com>
2014-11-20 17:29:46 -0200

    Adding git ignore, error and map pages and the new index

--- main.py
+++ main.py
@@ -38,10 +38,19 @@ class Place(ndb.Model):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        query = Place.query()
+        q = query.fetch(10)
+        places = []
+        s = ""
+        for p in q:
+            places.append(p.name)
+            s += p.name + " , "
+            s += str(p.latitude) + " , "
+            s += str(p.longitude) + "<br>"
+
         template_values = {
-            'orig' : (-23.558745, -46.731859, 'IMEUSP') ,
-            'waypts' : [(-23.572742, -46.696095, 'Shopping Eldorado')],
-            'dest' : (-23.561518, -46.656009, 'MASP')
+            'places' : places,
+            'placesString' : s
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -69,7 +78,46 @@ class ListStuff(webapp2.RequestHandler):
             s += str(p.longitude) + "<br>"
         self.response.write(s)
 
-class RandomTest(webapp2.RequestHandler):
+
+
+class ErrorHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'type' : str(self.request.get("type"))
+        }
+        template = JINJA_ENVIRONMENT.get_template('error.html')
+        self.response.write(template.render(template_values))
+
+class MapHandler(webapp2.RequestHandler):
+    def getPlaces(self, l):
+        places = []
+        q = Place.query()
+        for p in l:
+            if not (p == ''):
+                q2 = q.filter(Place.name == p)
+                places.append((q2.fetch()[0].latitude, q2.fetch()[0].longitude, p))
+        return places
+
+    def post(self):
+        req = str (self.request.get("locations"))
+        places = self.getPlaces(req.split('\t'))
+        waypts = []
+
+        if(len(places) < 2):
+            self.redirect("/error?type=400")
+        else:
+            for i in range(1, len(places)-1):
+                waypts.append(places[i])
+
+            template_values = {
+                'orig' : places[0],
+                'waypts' : waypts,
+                'dest' : places[-1]
+            }
+            template = JINJA_ENVIRONMENT.get_template('map.html')
+            self.response.write(template.render(template_values))
+
+class Populate(webapp2.RequestHandler):
     def get(self):
         lat = -23.558745
         lon = -46.731859
@@ -89,17 +137,19 @@ class RandomTest(webapp2.RequestHandler):
         place.put()
         lat = -23.572742
         lon = -46.696095
-        name = 'Shopping Eldorado'
+        name = 'ELDORADO'
         place = Place()
         place.latitude = lat
         place.longitude = lon
         place.name = name
         place.put()
-        self.response.write("Ok")
+        self.response.write("Populated")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/map', MapHandler),
     ('/register', RegisterStuff),
     ('/list', ListStuff),
-    ('/random', RandomTest)
+    ('/populate', Populate),
+    ('/error', ErrorHandler)
 ], debug=True)
